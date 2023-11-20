b23a474e100108bb60386e12613cd2cef8d21d37
zsharaf <zeyad.sharaf@zalando.de>
2023-11-20 18:14:59 +0100

    Adding project template

---
+++ src/test/java/com/example/tradingapplication/TradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tradingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
